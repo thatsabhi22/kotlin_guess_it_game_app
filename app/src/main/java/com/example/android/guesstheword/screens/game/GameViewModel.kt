1927ddbc1a82b548f100d86cc95325b9f2056b65
The Aviator <redefinedabhi2@gmail.com>
2021-01-26 20:06:48 +0530

    On the game finished navigation
    Navigate to Score Fragment, handling the onGameFinished action

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -22,8 +22,13 @@ class GameViewModel : ViewModel() {
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish : LiveData<Boolean>
+        get() = _eventGameFinish
+
     init{
         Timber.i("GameViewModel Created !!")
+        _eventGameFinish.value = false
         resetList()
         nextWord()
         _score.value = 0
@@ -40,7 +45,7 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-            //gameFinished()
+            _eventGameFinish.value = true
         } else {
             _word.value = wordList.removeAt(0)
         }
@@ -86,5 +91,9 @@ class GameViewModel : ViewModel() {
                 "bubble"
         )
         wordList.shuffle()
+    }
+
+    fun onGameFinishedComplete(){
+        _eventGameFinish.value = false
     }
 }
